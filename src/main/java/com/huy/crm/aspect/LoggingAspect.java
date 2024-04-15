7b37b98149b4ab86def1b0cd588c00ba0f94d245
Huy Trần <nhokpio95@gmail.com>
2024-04-15 20:03:13 +0700

    Update

--- src/main/java/com/huy/crm/aspect/LoggingAspect.java
+++ src/main/java/com/huy/crm/aspect/LoggingAspect.java
@@ -1,19 +1,19 @@
 package com.huy.crm.aspect;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class LoggingAspect {
 
-    private static final Logger logger = LogManager.getLogger(LoggingAspect.class);
+    private static final Logger logger = LoggerFactory.getLogger(LoggingAspect.class);
 
     @Pointcut("execution(* com.huy.crm.service.*.*(..))")
     private void forServicePackage() {
