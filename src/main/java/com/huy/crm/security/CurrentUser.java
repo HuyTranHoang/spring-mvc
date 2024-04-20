09937051b2d6a74e13480695569e04c65167eb15
Huy Trần <nhokpio95@gmail.com>
2024-04-20 22:56:32 +0700

    Update upload avatar for user

---
+++ src/main/java/com/huy/crm/security/CurrentUser.java
@@ -0,0 +1,25 @@
+package com.huy.crm.security;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+import java.util.Collection;
+
+public class CurrentUser extends User {
+
+    public CurrentUser(String username, String password, boolean enabled, boolean accountNonExpired,
+                       boolean credentialsNonExpired, boolean accountNonLocked,
+                       Collection<? extends GrantedAuthority> authorities) {
+        super(username, password, enabled, accountNonExpired, credentialsNonExpired, accountNonLocked, authorities);
+    }
+
+    private String imageUrl;
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
--- src/main/java/com/huy/crm/security/CustomUserDetailsService.java
+++ src/main/java/com/huy/crm/security/CustomUserDetailsService.java
@@ -1,7 +1,9 @@
 package com.huy.crm.security;
 
 import com.huy.crm.dao.UserDAO;
+import com.huy.crm.entity.Role;
 import com.huy.crm.entity.UserEntity;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -9,6 +11,8 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
@@ -20,20 +24,27 @@ public class CustomUserDetailsService implements UserDetailsService {
     }
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    public UserDetails loadUserByUsername(final String username) throws UsernameNotFoundException {
+
         UserEntity userEntity = userDAO.findByUserName(username);
+        return buildUserForAuthentication(userEntity, userEntity.getRoles().stream()
+                .map(role -> new SimpleGrantedAuthority(role.getName()))
+                .collect(Collectors.toList()));
+    }
 
-        if (userEntity == null) {
-            throw new UsernameNotFoundException("User not found");
-        }
+    private User buildUserForAuthentication(UserEntity user,
+                                            List<GrantedAuthority> authorities) {
+        String username = user.getUsername();
+        String password = user.getPassword();
+        boolean enabled = true;
+        boolean accountNonExpired = true;
+        boolean credentialsNonExpired = true;
+        boolean accountNonLocked = true;
 
-        return new User(
-                userEntity.getUsername(),
-                userEntity.getPassword(),
-                userEntity.getRoles()
-                        .stream()
-                        .map(role -> new SimpleGrantedAuthority(role.getName()))
-                        .collect(Collectors.toList())
-        );
+        CurrentUser currentUser = new CurrentUser(username, password, enabled, accountNonExpired, credentialsNonExpired,
+                accountNonLocked, authorities);
+
+        currentUser.setImageUrl(user.getImageUrl());
+        return currentUser;
     }
 }
